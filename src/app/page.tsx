609574a1be77e040161f54e00539b8506ce4f7d7
ayushsth123 <ayyush227@gmail.com>
2025-05-18 22:45:25 +0545

    globalcss changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import MaxWidthWrapper from "./MaxWidthWrapper";
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
 
 export default function Home() {
   return (
     <div>
-      <MaxWidthWrapper className="bg-red-500">
-        Hello world
+      <MaxWidthWrapper className="mb-12 nt-28 sm:mt-40 flex flex-col items-center justify-between text-center">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border bg-white border-neutral-200 px-7 py-2 shadow-md backdrop-blur-md transition-all hover:border-neutral-300 hover:bg-white/50">
+        <p className="text-sm font-semibold text-neutral-700">
+          Hello how are yu
+        </p>
+        </div>
       </MaxWidthWrapper>
     </div>
   );
