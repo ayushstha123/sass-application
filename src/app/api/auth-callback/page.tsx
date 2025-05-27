34e6a6fd432fe0a673c93964c88036ae6afd32d1
ayushsth123 <ayyush227@gmail.com>
2025-05-27 08:17:04 +0545

    tRPC setup

--- src/app/api/auth-callback/page.tsx
+++ src/app/api/auth-callback/page.tsx
@@ -1,12 +1,21 @@
+import { trpc } from '@/app/_trpc/client'
 import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const page = async() => {
     const router = useRouter()
     const searchParams = useSearchParams()
     const origin = searchParams.get('origin')
-
-  return (
+    
+    const {data,isLoading}=trpc.authCallback.useQuery(undefined,{
+      onSuccess:({success}:{success:boolean})=>{
+        if(success){
+          //user is synced to db
+          router.push(origin ? `/${origin}`:'/dashboard')
+        }
+      }
+    })
+    return (
     <div>page</div>
   )
 }
